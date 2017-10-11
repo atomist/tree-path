81c875bb59e73022b5fce2c462f51daa12381a66
Rod Johnson <rod@atomist.com>
2017-10-11 15:53:23 -0700

    Added tests for enrichment

---
+++ test/tree/dynamicPropertiesTest.ts
@@ -0,0 +1,30 @@
+import "mocha";
+
+import * as assert from "power-assert";
+import { defineDynamicProperties } from "../../src/tree/enrichment";
+import { TreeNode } from "../../src/tree/TreeNode";
+
+describe("dynamic property enrichment", () => {
+
+    it("should enrich node with 2 terminal scalar children, exposing terminal values", () => {
+        const a: TreeNode = {$name: "a", $value: "A"};
+        const b: TreeNode = {$name: "b", $value: "B"};
+        const parent: any = {$name: "foo", $children: [a, b]};
+        assert(!parent.a);
+        defineDynamicProperties(parent);
+        assert(!!parent.a);
+        assert(parent.a === a.$value);
+    });
+
+    it("should enrich node with non-terminal child", () => {
+        const grandkid: TreeNode = {$name: "grandkid", $value: "B"};
+        const kid: TreeNode = {$name: "kid", $children: [grandkid]};
+        const parent: any = {$name: "foo", $children: [kid]};
+
+        assert(!parent.kid);
+        defineDynamicProperties(parent);
+        assert(!!parent.kid);
+        assert(parent.kid === kid);
+        assert(parent.kid.grandkid === "B");
+    });
+});
---
+++ test/tree/fillInValuesTest.ts
@@ -0,0 +1,33 @@
+import "mocha";
+
+import * as assert from "power-assert";
+import { fillInEmptyNonTerminalValues } from "../../src/tree/enrichment";
+import { TreeNode } from "../../src/tree/TreeNode";
+
+describe("fill in values", () => {
+
+    it("should fill in value with 2 terminal scalar children, exposing terminal values", () => {
+        const input = "AB";
+        const a: TreeNode = {$name: "a", $value: "A", $offset: 0};
+        const b: TreeNode = {$name: "b", $value: "B", $offset: 1};
+        const parent: any = {$name: "foo", $children: [a, b], $offset: 0 };
+        assert(!parent.$value);
+        fillInEmptyNonTerminalValues(parent, input);
+        assert(!!parent.$value);
+        assert(parent.$value === input);
+    });
+
+    it("should enrich node with non-terminal child", () => {
+        const input = "B";
+        const grandkid: TreeNode = {$name: "grandkid", $value: "B", $offset: 0};
+        const kid: TreeNode = {$name: "kid", $children: [grandkid], $offset: 0};
+        const parent: any = {$name: "foo", $children: [kid], $offset: 0};
+
+        assert(!parent.$value);
+        fillInEmptyNonTerminalValues(parent, input);
+        assert(!!parent.$value);
+        assert(!!kid.$value);
+        assert(parent.$value === input);
+        assert(kid.$value === input);
+    });
+});
