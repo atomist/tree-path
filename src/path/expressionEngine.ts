bd2bf245302db84fba7cd4a3255899f376feaf4a
Rod Johnson <rod@atomist.com>
2017-10-16 18:30:44 -0700

    Fixed #3: Path expression evaluation can take a string

--- src/path/expressionEngine.ts
+++ src/path/expressionEngine.ts
@@ -2,21 +2,23 @@ import { TreeNode } from "../TreeNode";
 import { ExecutionResult, isSuccessResult, PathExpression } from "./pathExpression";
 
 import * as _ from "lodash";
+import { toPathExpression } from "./pathExpressionParser";
 
-export type ExpressionEngine = (node: TreeNode, parsed: PathExpression) => ExecutionResult;
+export type ExpressionEngine = (node: TreeNode, parsed: PathExpression | string) => ExecutionResult;
 
 /**
  * Return the result of evaluating the expression. If the expression is invalid
  * return a message, otherwise the result of invoking the valid expression.
  *
  * @param root  root node to evaluateExpression the path against
- * @param pex   Parsed path expression. It's already been validated
+ * @param pex   Parsed or string path expression.
  * @return
  */
 export function evaluateExpression(root: TreeNode,
-                                   pex: PathExpression): ExecutionResult {
+                                   pex: string | PathExpression): ExecutionResult {
+    const parsed = toPathExpression(pex);
     let currentResult: ExecutionResult = [root];
-    for (const locationStep of pex.locationSteps) {
+    for (const locationStep of parsed.locationSteps) {
         if (isSuccessResult(currentResult)) {
             if (currentResult.length > 0) {
                 const allNextNodes =
