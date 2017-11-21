cb2590a5b005d597e8a3e78f9107a28939060bb2
Rod Johnson <rod@atomist.com>
2017-11-21 15:17:12 +1100

    Add allNodeTests convenience function

--- src/internal/pathExpressionUtils.ts
+++ src/internal/pathExpressionUtils.ts
@@ -1,9 +1,15 @@
 import * as _ from "lodash";
 
-import { isUnionPathExpression, PathExpression, Predicate } from "../path/pathExpression";
+import { isUnionPathExpression, NodeTest, PathExpression, Predicate } from "../path/pathExpression";
 
 export function allPredicates(pe: PathExpression): Predicate[] {
     return isUnionPathExpression(pe) ?
         _.flatten(pe.unions.map(u => allPredicates(u))) :
         _.flatten(pe.locationSteps.map(s => s.predicates));
+}
+
+export function allNodeTests(pe: PathExpression): NodeTest[] {
+    return isUnionPathExpression(pe) ?
+        _.flatten(pe.unions.map(u => allNodeTests(u))) :
+        _.flatten(pe.locationSteps.map(s => s.test));
 }
